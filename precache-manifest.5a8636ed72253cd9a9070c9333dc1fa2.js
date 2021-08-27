6305fad8a9ab9fe354250d1154574d7befc16bf8
Kuldeep Patel <kuldeep.patel@paytm.com>
2021-08-28 01:16:12 +0530

    Updates

---
+++ precache-manifest.5a8636ed72253cd9a9070c9333dc1fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e158e0d2f0f02f257de58ba5fa84d09b",
+    "url": "/trello-board/index.html"
+  },
+  {
+    "revision": "47404fc68557617026d8",
+    "url": "/trello-board/static/css/main.8e18de48.chunk.css"
+  },
+  {
+    "revision": "b6d56e93918cb09d1f34",
+    "url": "/trello-board/static/js/2.30a050e9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trello-board/static/js/2.30a050e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47404fc68557617026d8",
+    "url": "/trello-board/static/js/main.6dfa37fa.chunk.js"
+  },
+  {
+    "revision": "8a0375ac14b060ed879c",
+    "url": "/trello-board/static/js/runtime-main.f4466e66.js"
+  }
+]);
